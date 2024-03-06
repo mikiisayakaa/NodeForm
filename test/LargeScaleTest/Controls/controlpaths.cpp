043786907f760bfc0730a78be3cdd2b88bfb94ce
Peng Yan <1105012124@qq.com>
2024-03-06 23:11:30 +0800

    remove the naive test

--- test/LargeScaleTest/Controls/controlpaths.cpp
+++
@@ -1,7 +0,0 @@
-#include "controlpaths.h"
-
-namespace NF{
-
-QString buttonPath = "D:/C++/NF/NF/test/LargeScaleTest/Controls/ButtonType1.qml";
-
-}
--- test/LargeScaleTest/Controls/controlpaths.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <QString>
-
-namespace NF{
-
-extern QString buttonPath;
-
-}
